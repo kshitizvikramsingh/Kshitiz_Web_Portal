fb2b00c0aa971c960207dcf2c9dee51502f936e6
kshitiz vikram singh <kshitizvikramsingh123@gmail.com>
2023-05-02 21:55:49 +0530

    changed loading

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,20 +1,20 @@
-// document.onreadystatechange = function() {
-//     if (document.readyState !== "complete" ) {
-//         document.querySelector("body").style.visibility = "hidden";
-//         document.querySelector("#loader").style.visibility = "visible";
-//     } else {
-//         document.querySelector("#loader").style.display = "none";
-//         document.querySelector("body").style.visibility = "visible";
-//     }
-// };
-
-document.onreadystatechange = function () {
-    var state = document.readyState
-    if (state == 'complete') {
-        setTimeout(function(){
-            
-           document.getElementById('loader').style.display="none";
-        },2000);
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete" ) {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
     }
-  }
+};
+
+// document.onreadystatechange = function () {
+//     var state = document.readyState
+//     if (state == 'complete') {
+//         setTimeout(function(){
+            
+//            document.getElementById('loader').style.display="none";
+//         },1000);
+//     }
+//   }
 
