b8217d00e4da8fea95bf0dea2aef2d3f50506806
kshitiz vikram singh <kshitizvikramsingh123@gmail.com>
2023-05-02 21:03:56 +0530

    loader icon

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,19 +1,20 @@
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }
-};
-
-// document.onreadystatechange = function () {
-//     var state = document.readyState
-//     if (state == 'complete') {
-//         setTimeout(function(){
-//             document.getElementById('interactive');
-//            document.getElementById('loader').style.visibility="hidden";
-//         },1000);
+// document.onreadystatechange = function() {
+//     if (document.readyState !== "complete" ) {
+//         document.querySelector("body").style.visibility = "hidden";
+//         document.querySelector("#loader").style.visibility = "visible";
+//     } else {
+//         document.querySelector("#loader").style.display = "none";
+//         document.querySelector("body").style.visibility = "visible";
 //     }
-//   }
+// };
+
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'complete') {
+        setTimeout(function(){
+            
+           document.getElementById('loader').style.display="none";
+        },5000);
+    }
+  }
+
